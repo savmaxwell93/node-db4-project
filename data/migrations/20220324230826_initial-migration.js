2a3565a5b58effddb8f71b207718aaddc4700551
Savannah <savmaxwell@gmail.com>
2022-03-24 17:12:16 -0600

    ready to build tables

---
+++ data/migrations/20220324230826_initial-migration.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('recipes')
+    .createTable('ingredients')
+    .createTable('steps')
+    .createTable('step_ingredients')
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema
+    .dropTableIfExists('step_ingredients')
+    .dropTableIfExists('steps')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes')
+};
